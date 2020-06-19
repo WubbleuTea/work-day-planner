85f72e74c1233c49ac70bbc356206637943b7814
Joshua Weisbrod-Torres <64339271+joshuaweisbrodtorres@users.noreply.github.com>
2020-06-19 17:49:31 -0500

    clicking happens

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,25 @@ var now = moment().format("dddd, MMMM Do, YYYY");
 //adds current day to the page
 $("#currentDay").append("<p class='body'>" + now + "</p>");
 
+//on click of text field
+$(".hour-group").on("click", ".textarea", function(){
+    console.log("Clicked on where the text area should go")
+    
+  
+  });
+
+  $(".hour-group").on("blur", ".textarea", function(){
+      console.log("this has been blurred")
+ 
+      
+  });
+//save button functionality
+$(".hour-group").on("click", ".saveBtn", function(){
+    console.log("Clicked on saveBtn")
+
+
+  });
+
+  //need jquery to test every time to see if the current time is past the time we currently on?
+    // data ids? data-hour = #?
+  
