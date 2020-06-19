dc2f5662d54e59b7a696418f95d7a04c3daadded
Joshua Weisbrod-Torres <64339271+joshuaweisbrodtorres@users.noreply.github.com>
2020-06-19 16:54:54 -0500

    Timeblocks created

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+//creates a variable to store current date
 var now = moment().format("dddd, MMMM Do, YYYY");
 
+//adds current day to the page
 $("#currentDay").append("<p class='body'>" + now + "</p>");
+
